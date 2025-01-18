413bacb4700cb894e9d6b34fbd74649332389e77
Andrew Yeung <71157672+Andrew-Yeung@users.noreply.github.com>
2025-01-18 13:51:10 -0500

    JSON

--- UTEK/src/Main.java
+++ UTEK/src/Main.java
@@ -1,8 +1,23 @@
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Iterator;
+import java.util.Map;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.*;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException, ParseException {
         //read the json file
+        JSONParser jsonparse = new JSONParser();
 
+        JSONObject jsonobject = (JSONObject) jsonparse.parse(new FileReader("UTEK/src/data.json"));
+
+        // read data
+        String name = (String) jsonobject.get("name");
+        System.out.println(name);
 
     }
 }
